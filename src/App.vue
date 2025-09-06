898d67f5aac3a424e64075b4b0db8d54f2fae1d2
mg33550336 <maciej1gruca@gmail.com>
2025-09-06 16:45:53 +0200

    Added additional counts and computable

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
-<script setup></script>
+<script setup>
+import {ref} from 'vue'
+import {computed} from 'vue'
+
+const product = ref('Socks')
+const count = ref(0)
+const description = ref('Product description')
+const stock = ref(10)
+
+const remainingStock = computed(() => stock.value - count.value)
+
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{count}} {{product}}</h1>
+        <button @click="count++" :disabled="remainingStock === 0">Increase</button>
+        <button @click="count--" :disabled="count === 0">Decrease</button>
+      
+        <p>Stock: {{ remainingStock }}</p>
       </div>
     </div>
   </div>
